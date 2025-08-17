1a954d2bf82f00cff599a655f9e0bdf288fde93e
mojokojoxd <kwadwoneer@gmail.com>
2025-08-17 10:16:46 -0600

    changing call to action page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,24 @@ export default function Home()
 {
   return (
     <div className="h-fit w-full px-[5%] lg:px-[6.5%] home-categories bg-cover">
-      <section className='h-full w-full pt-16 sm:pt-24 xl:grid grid-cols-2 gap-5'>
+      <section className='h-full w-full max-w-7xl mx-auto pt-16 sm:pt-24 xl:grid grid-cols-2'>
         <div className='pb-24 space-y-8'>
-          <div className='leading-loose text-xl'>
+          <div className='leading-loose text-base'>
             <h1 className='text-6xl sm:text-8xl md:text-9xl lg:text-7xl font-bold tracking-tighter text-zinc-800 mb-4'>Just Moved?</h1>
             <div className='leading-normal mb-4'>
               <p className='max-w-lg'>Congratulations! We&apos;ll help setup your new home in minutes. Pick a Service - It&apos;s Free.</p>
             </div>
-            <Link href={ '/marketplace' } className='block'>
-              <h2 className='text-primary'>Save on Home Items</h2>
+            <Link href={ '/marketplace' } className='block mb-4'>
+              <h2 className='text-primary text-2xl font-semibold'>Save on Home Items</h2>
               <p className='leading-normal max-w-md'>Get Deals on furniture, décor and essentials nearby.</p>
             </Link>
-            <Link href={ '/survey-projects/base' } className='block'>
-              <h2 className='text-primary'>Get project quotes</h2>
+            <Link href={ '/survey-projects/base' } className='block mb-4'>
+              <h2 className='text-primary text-2xl font-semibold'>Get project quotes</h2>
               <p className='leading-normal max-w-md'>Planning a big job like pools, HVAC, pest control or landscaping? Get bids from top-rated pros.
               </p>
             </Link>
-            <Link href={ '/handyman' } className='block'>
-              <h2 className='text-primary'>Hire Handyman</h2>
+            <Link href={ '/handyman' } className='block mb-4'>
+              <h2 className='text-primary text-2xl font-semibold'>Hire Handyman</h2>
               <p className='leading-normal max-w-md'>Need help with a small job e.g. mount tv, clean home, install fans or blinds, movers? Connect with our vetted and highly rated vendors
               </p>
             </Link>
@@ -59,6 +59,14 @@ export default function Home()
                 </div>
               </div>
             </Link>
+            <Link href={ '/survey-projects/hvac-plumbing' }>
+              <div className='group relative w-full aspect-square rounded-xl shadow-lg overflow-hidden ring-1 ring-rose-300'>
+                <Image src={ 'https://images.pexels.com/photos/32497161/pexels-photo-32497161.jpeg' } alt='security service' fill className='object-cover object-center transition-[transform] duration-300 ease-in-out group-hover:scale-110' />
+                <div className='absolute bottom-0 inset-x-0 h-1/4 flex justify-center items-center text-base text-neutral-foreground font-medium'>
+                  <span className='bg-neutral rounded-xl px-2.5'>HVAC & Plumbing</span>
+                </div>
+              </div>
+            </Link>
           </div>
         </div>
         <div className='hidden lg:grid pb-24 md:grid-cols-4 xl:grid-cols-3 auto-cols-fr gap-4'>
@@ -79,7 +87,7 @@ export default function Home()
             </div>
           </Link>
           <Link href={ '/survey-projects/landscaping' }>
-            <div className='group relative w-full aspect-square rounded-full shadow-lg overflow-hidden ring-1 ring-rose-300'>
+            <div className='group relative w-full aspect-square rounded-xl shadow-lg overflow-hidden ring-1 ring-rose-300'>
               <Image src={ 'https://images.unsplash.com/photo-1605117882932-f9e32b03fea9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFuZHNjYXBpbmd8ZW58MHx8MHx8fDA%3D' } alt='landscaping service' fill className='object-cover object-center transition-[transform] duration-300 ease-in-out group-hover:scale-110' />
               <div className='absolute bottom-0 inset-x-0 h-1/4 flex justify-center items-center text-base text-neutral-foreground font-medium'>
                 <span className='bg-neutral rounded-xl px-2.5'>Landscaping</span>
